05953c601c80d4eb374f2411180fc3fc6cebf715
rujutha <bhavnabhansali01@gmail.com>
2021-01-03 15:42:50 +0530

    car

--- js/Player.js
+++ js/Player.js
@@ -3,6 +3,10 @@ class Player {
     this.index = null;
     this.distance = 0;
     this.name = null;
+this.rank=null;
+
+
+
   }
 
   getCount(){
@@ -32,4 +36,19 @@ class Player {
       allPlayers = data.val();
     })
   }
+
+getCarsAtEnd(){
+database.ref('CarsAtEnd').on("value",(data)=>{
+  this.rank=data.val();
+})
+
+
+}
+
+static updateCarsAtEnd(rank){
+  database.ref('/').update({
+    CarsAtEnd:rank
+  })
+}
+
 }
